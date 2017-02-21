45570f5fb2cd037ccb9f6a7c43ab630fde9d5d43
Kenneth Tang <kennethtang@Kenneths-MacBook-Pro.local>
2017-02-21 22:18:36 +0800

    initial Setup

---
+++ app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule }														from '@angular/core';
+import { BrowserModule }											from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule }		from '@angular/forms';
+import { NgbModule }													from '@ng-bootstrap/ng-bootstrap';
+import { HttpModule }													from '@angular/http';
+
+import { AppComponent }												from './app.component';
+import { HomeComponent } 											from './home/home.component';
+// import { NavbarComponent } from './navbar/navbar.component';
+ 
+import { AppRoutingModule } from './app-routing.module'; // AppRoutingModule come last!
+
+@NgModule({
+	imports: [
+		BrowserModule,
+		FormsModule,
+		ReactiveFormsModule,
+		NgbModule.forRoot(),
+		HttpModule,
+		AppRoutingModule
+	],
+	declarations: [
+		AppComponent,
+		HomeComponent,
+		// NavbarComponent
+	],
+	providers: [
+
+	],
+	bootstrap: [
+		AppComponent
+	]
+})
+export class AppModule {}
+
+
